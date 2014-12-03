1716e8ddbace7f93eb204a8c0ca4a021cf657157
ZhongminWu <vwzm@live.com>
2014-12-03 22:03:26 +0800

    Add RGSS engine

---
+++ GameRGSSLibrary.Windows/GameEngine/RGSSEngine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoGame.Framework;
+using Windows.Foundation;
+using Windows.System.Threading;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace GameRGSSLibrary.Windows.GameEngine
+{
+    public delegate int RGSSEntry();
+    enum EngineState
+    {
+        UnInitialized,
+        Initialized,
+        Runing,
+    }
+    public class RGSSEngine
+    {
+        private static RGSSGame game;
+        private static RGSSEntry EntryCallback;
+        private static EngineState state = EngineState.UnInitialized;
+        public static void Init(string launchParameters, CoreWindow window, SwapChainBackgroundPanel swapChainBackgroundPanel)
+        {
+            if (state != EngineState.UnInitialized)
+            {
+                game = XamlGame<RGSSGame>.Create(launchParameters, window, swapChainBackgroundPanel);
+                game.IsFixedTimeStep = true;
+                game.GameControler = (GameControler.CreateControler(game));
+            }
+            else
+                throw new Exception("RGSSEngine has already been initialized");
+        }
+
+        public static Task Run(RGSSEntry entry)
+        {
+            if (state != EngineState.Runing)
+            {
+                EntryCallback = entry;
+                IAsyncAction asyncAction = ThreadPool.RunAsync(Worker, WorkItemPriority.High);
+                return asyncAction.AsTask();
+            }
+            return null;
+        }
+
+        public static void Worker(IAsyncAction action)
+        {
+            EntryCallback();
+        }
+
+
+    }
+}
---
+++ GameRGSSLibrary.Windows/GameEngine/RGSSGame.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameRGSSLibrary.Windows.GameEngine
+{
+    public class RGSSGame:Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        GameControler _gameControler;
+        public RGSSGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        public GameControler GameControler
+        {
+            get
+            {
+                return _gameControler;
+            }
+            set
+            {
+                _gameControler = value;
+            }
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            switch(_gameControler.GameState)
+            {
+                case GameState.Freezed:
+                    break;
+                case GameState.Runging:
+                    //drawing context;
+                    _gameControler.DrawOneFrameCompleted.Set();
+                    break;
+                case GameState.Transacting:
+                    _gameControler.DrawOneFrameCompleted.Set();
+                    break;
+                default:
+                    break;
+            }
+            base.Draw(gameTime);
+        }
+    }
+}
